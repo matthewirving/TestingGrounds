e133f1eaeb4e4b7d51df853db908fd94c5b17ca9
Mirving <matthewirving135@gmail.com>
2022-12-03 01:34:51 -0500

    Modified project configurations, and started to begin work on main class

--- src/main.c
+++ src/main.c
@@ -1,1 +1,21 @@
-void app_main() {}
+#include <Arduino.h>
+#include <Wifi.h>
+#include <WebServer.h>
+#include "../credentials/info.h"
+#include <iostream>
+
+
+
+
+
+
+void setup() //put setup code here to run once
+{
+
+}
+
+
+void loop() //put code you want run continuously here
+{
+
+}
